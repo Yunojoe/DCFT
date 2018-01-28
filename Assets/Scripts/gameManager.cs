54db5c392b7aa6a147ac5ef66a0b38ccf72efdea
Yunojoe <joe.ford@yunomedia.com>
2018-01-28 02:00:07 +0000

    Started Mapgen

---
+++ Assets/Scripts/gameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameManager : MonoBehaviour
+{
+
+    private void Start(){
+        BeginGame();
+    }
+
+    private void Update(){
+        if (Input.GetKeyDown(KeyCode.Space)) //Todo - change the "reset" key to something better or even a menu button
+        {
+            RestartGame();
+        }
+    }
+
+    public mapGenerator dungeonPrefab;
+    private mapGenerator dungeonInstance;
+
+    private void BeginGame() {
+        dungeonInstance = Instantiate(dungeonPrefab) as mapGenerator;
+        dungeonInstance.Generate();
+    }
+
+    private void RestartGame() {
+
+    }
+}
